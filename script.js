2b387ae28fec9a3c009dd667b1ffe54b6753f4ab
Vanjula <vanjulasankar@gmail.com>
2024-06-02 14:57:00 +0530

    updation

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-let input=document.getElementById("inputbox");
-let string="";
+let input = document.getElementById("inputbox");
+let string = "";
 
-let button=document.querySelectorAll("button");
+let button = document.querySelectorAll("button");
 
-let arr=Array.from(button);
-arr.forEach(button=>{
-button.addEventListener('click',(e)=>{
-
-    if(e.target.innerHTML=='='){
-string =eval(string);
-input.value=string;
-    }
- 
-     else if(e.target.innerHTML=='AC'){
-        string="";
-        input.value=string;
-     }
-
-     else if(e.target.innerHTML=='DEL'){
-           string=string.substring(0,string.length-1);
-           input.value=string;
-     }
-     else{
-        string+=e.target.innerHTML;
-        input.value=8;
-     }
-})
-})
-
+let arr = Array.from(button);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        } else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        } else {
+            string += e.target.innerHTML;
+            input.value = string; 
+        }
+    });
+});
 
